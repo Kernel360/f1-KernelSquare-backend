907394013a0df9211cc010447f03261537cc58be
hodako <kbry12@naver.com>
2023-12-11 12:06:42 +0900

    project init

---
+++ src/main/java/com/kernel360/kernelsquare/KernelsquareApplication.java
@@ -0,0 +1,13 @@
+package com.kernel360.kernelsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernelsquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KernelsquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kernel360/kernelsquare/KernelsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel360.kernelsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernelsquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

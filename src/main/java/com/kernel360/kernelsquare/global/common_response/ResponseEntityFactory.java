0c32829b5e2b6f0fc2a9dc95ad2504efeabe8769
hodako <kbry12@naver.com>
2023-12-21 12:03:29 +0900

    refactor: MemberController refactoring 모든 ReponseEntity를 ResponseEntityFactory에서 생성하도록 변경

--- src/main/java/com/kernel360/kernelsquare/global/common_response/ResponseEntityFactory.java
+++ src/main/java/com/kernel360/kernelsquare/global/common_response/ResponseEntityFactory.java
@@ -2,12 +2,12 @@ package com.kernel360.kernelsquare.global.common_response;
 
 import org.springframework.http.ResponseEntity;
 
-public class ResponseEntityFactory<T> {
-	public static ResponseEntity<ApiResponse> of(StatusCode statusCode) {
+public class ResponseEntityFactory {
+	public static ResponseEntity<ApiResponse> toResponseEntity(StatusCode statusCode) {
 		return ResponseEntity.status(statusCode.getStatus()).body(ApiResponse.of(statusCode));
 	}
 
-	public static <T> ResponseEntity<ApiResponse<T>> of(StatusCode statusCode, T data) {
+	public static <T> ResponseEntity<ApiResponse<T>> toResponseEntity(StatusCode statusCode, T data) {
 		return ResponseEntity.status(statusCode.getStatus()).body(ApiResponse.of(statusCode, data));
 	}
 }

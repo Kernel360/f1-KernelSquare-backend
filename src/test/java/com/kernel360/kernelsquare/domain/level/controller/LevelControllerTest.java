c97bd4d58a7a66e51288329df87eb3054acd005f
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-12-29 19:08:40 +0900

    #32 등급 생성 API 완료

--- src/test/java/com/kernel360/kernelsquare/domain/level/controller/LevelControllerTest.java
+++ src/test/java/com/kernel360/kernelsquare/domain/level/controller/LevelControllerTest.java
@@ -13,8 +13,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static com.kernel360.kernelsquare.global.common_response.response.code.LevelResponseCode.LEVEL_CREATED;
 import static org.junit.jupiter.api.Assertions.*;

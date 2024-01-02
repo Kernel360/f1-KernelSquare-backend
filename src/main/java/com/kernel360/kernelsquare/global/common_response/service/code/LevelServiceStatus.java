0076c2ec70643b79cf911f30f643b96ce07289a9
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-01-02 14:11:56 +0900

    등급 생성 응답코드 수정

--- src/main/java/com/kernel360/kernelsquare/global/common_response/service/code/LevelServiceStatus.java
+++ src/main/java/com/kernel360/kernelsquare/global/common_response/service/code/LevelServiceStatus.java
@@ -5,11 +5,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum LevelServiceStatus implements ServiceStatus {
     //error
-    LEVEL_NOT_FOUND(1300),
-    LEVEL_ALREADY_EXISTED(1301),
+    LEVEL_NOT_FOUND(1400),
+    LEVEL_ALREADY_EXISTED(1401),
 
     //success
-    LEVEL_CREATED(1340);
+    LEVEL_CREATED(1440);
 
     private final Integer code;
 

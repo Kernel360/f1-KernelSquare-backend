dec854d18950173e7747bba56d95bdea3104a6fa
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-01-02 18:28:46 +0900

    #34 등급조회 기능(테스트 포함)

--- src/test/java/com/kernel360/kernelsquare/domain/level/controller/LevelControllerTest.java
+++ src/test/java/com/kernel360/kernelsquare/domain/level/controller/LevelControllerTest.java
@@ -3,6 +3,7 @@ package com.kernel360.kernelsquare.domain.level.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kernel360.kernelsquare.domain.level.dto.CreateLevelRequest;
 import com.kernel360.kernelsquare.domain.level.dto.CreateLevelResponse;
+import com.kernel360.kernelsquare.domain.level.dto.FindAllLevelResponse;
 import com.kernel360.kernelsquare.domain.level.entity.Level;
 import com.kernel360.kernelsquare.domain.level.service.LevelService;
 import org.junit.jupiter.api.DisplayName;
@@ -14,12 +15,18 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.kernel360.kernelsquare.global.common_response.response.code.LevelResponseCode.LEVEL_CREATED;
+import static com.kernel360.kernelsquare.global.common_response.response.code.LevelResponseCode.LEVEL_FOUND;
+import static com.kernel360.kernelsquare.global.common_response.response.code.TechStackResponseCode.TECH_STACK_CREATED;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("등급 컨트롤러 통합 테스트")
@@ -43,7 +50,7 @@ class LevelControllerTest {
 
         Level level = new Level(name, imageUrl);
         CreateLevelRequest createLevelRequest = new CreateLevelRequest(name, imageUrl);
-        CreateLevelResponse createLevelResponse = CreateLevelResponse.of(level);
+        CreateLevelResponse createLevelResponse = CreateLevelResponse.from(level);
 
         doReturn(createLevelResponse)
                 .when(levelService)
@@ -68,6 +75,48 @@ class LevelControllerTest {
         verify(levelService, times(1)).createLevel(any(CreateLevelRequest.class));
 
 
+    }
+
+//    @Test
+//    @DisplayName("등급 조회 성공시 200 OK와 응답 메시지를 반환한다")
+//    void testFindAllLevel() throws Exception {
+//        // Given
+//        Long
+//
+//        // When
+//
+//        // Then
+//    }
+
+    @Test
+    @DisplayName("모든 레벨 조회 성공시 200 OK와 레벨 목록을 반환한다")
+    void testFindAllLevel() throws Exception {
+        // given
+        List<Level> levelList = Arrays.asList(
+                Level.builder().name(1L).imageUrl("image1.jpg").build(),
+                Level.builder().name(2L).imageUrl("image2.jpg").build()
+        );
+        FindAllLevelResponse response = FindAllLevelResponse.from(levelList);
+
+        doReturn(response)
+                .when(levelService)
+                .findAllLevel();
+
+        // when & then
+        mockMvc.perform(get("/api/v1/levels")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().is(LEVEL_FOUND.getStatus().value()))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value(LEVEL_FOUND.getCode()))
+                .andExpect(jsonPath("$.msg").value(LEVEL_FOUND.getMsg()))
+                .andExpect(jsonPath("$.data.levels[0].image_url").value(levelList.get(0).getImageUrl()));
+//                .andExpect(jsonPath("$.data.levels[1]").value(levelList.get(1)));
+
+        // verify
+        verify(levelService, times(1)).findAllLevel();
     }
 
 }

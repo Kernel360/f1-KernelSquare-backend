e9c024e9bd61634fb000e709e818b24722bd0003
mooncw <dusrktl4@gmail.com>
2024-01-05 16:44:37 +0900

    [refactor] parseFilePath 리팩토링
    - 중복 파일 이름으로 인한 기존 파일이 의도치 않게 지워지지 않도록 유니크한 filePath를 갖도록 수정
    - 1차 수정이고 좋은 의견 있으시면 말씀해주세요.

--- src/main/java/com/kernel360/kernelsquare/domain/image/utils/ImageUtils.java
+++ src/main/java/com/kernel360/kernelsquare/domain/image/utils/ImageUtils.java
@@ -3,10 +3,15 @@ package com.kernel360.kernelsquare.domain.image.utils;
 import com.kernel360.kernelsquare.domain.image.component.ImageComponent;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 public class ImageUtils {
 
+    // 클라이언트에게 받은 이미지 파일을 받아서 s3에 저장할 file path를 만듦
     public static String makeFilePath(String category, MultipartFile multipartFile) {
-        return category + "/" + multipartFile.getOriginalFilename();
+        UUID uuid = UUID.randomUUID();
+
+        return category + "/" + uuid + multipartFile.getOriginalFilename();
     }
 
     public static String parseFilePath(String url) {

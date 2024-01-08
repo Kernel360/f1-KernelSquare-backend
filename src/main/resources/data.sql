01198ba86b6b132f6dad2c2f072d002280ed741d
mooncw <dusrktl4@gmail.com>
2024-01-08 22:27:19 +0900

    [bugfix] Level test 에러부분 수정
    - Level imageURL에서 s3 주소 대신 null로 넣어 처리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 INSERT INTO member (created_date, experience, level_id, modified_date, email, image_url, nickname, password,
                     introduction)
-VALUES ('2023-12-19', 1, 1, '2023-12-19', 'email@example.com', 'http://example.com/image.jpg', 'nickname', 'password',
+VALUES ('2023-12-19T09:00:00', 1, 1, '2023-12-19T09:00:00', 'email@example.com', 'example/image.jpg', 'nickname', 'password',
         'introduction');
 
 INSERT INTO question (closed_status, created_date, id, member_id, modified_date, view_count, content, image_url, title)
-VALUES (1, '2023-12-19', 1, 1, '2023-12-21', 20, 'content', 'http://example.com/image.jpg', 'question title');
+VALUES (1, '2023-12-19T09:00:00', 1, 1, '2023-12-21T09:00:00', 20, 'content', 'example/image.jpg', 'question title');
 
 INSERT INTO tech_stack (created_date, modified_date, skill)
-VALUES ('2023-12-19', '2023-12-19', 'JavaScript');
+VALUES ('2023-12-19T09:00:00', '2023-12-19T09:00:00', 'JavaScript');
 
 INSERT INTO tech_stack (created_date, modified_date, skill)
-VALUES ('2023-12-19', '2023-12-19', 'Python');
+VALUES ('2023-12-19T09:00:00', '2023-12-19T09:00:00', 'Python');
 
 INSERT INTO level (created_date, modified_date, image_url, name)
-VALUES ('2023-12-12', '2023-12-12', 'level/level1to2.png', 1);
+VALUES ('2023-12-12T09:00:00', '2023-12-12T09:00:00', 'level/level1to2.png', 1);
 
 INSERT INTO authority (authority_type)
 VALUES ('ROLE_USER');

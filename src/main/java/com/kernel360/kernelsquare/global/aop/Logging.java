b8e172429003951eb02ab1774d9dd05fe93286aa
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-01-09 15:56:55 +0900

    AOP 추가작업 - 질문,답변,투표,멤버,이미지 서비스와 레포지터리 추가

--- src/main/java/com/kernel360/kernelsquare/global/aop/Logging.java
+++ src/main/java/com/kernel360/kernelsquare/global/aop/Logging.java
@@ -14,15 +14,19 @@ import org.springframework.stereotype.Component;
 public class Logging {
     //ToDo 로그 정책이 정해지면 리팩토링
     @Pointcut("execution(* com.kernel360.kernelsquare.domain.member.service.MemberService.*(..)) || " +
-        "execution(* com.kernel360.kernelsquare.domain.question.service.QuestionService.*(..))")
-    public void allController() {}
+            "execution(* com.kernel360.kernelsquare.domain.question.service.QuestionService.*(..)) || " +
+            "execution(* com.kernel360.kernelsquare.domain.answer.service.AnswerService.*(..)) || " +
+            "execution(* com.kernel360.kernelsquare.domain.member_answer_vote.service.MemberAnswerVoteService.*(..)) || " +
+            "execution(* com.kernel360.kernelsquare.domain.image.service.ImageService.*(..))")
+    public void allController() {
+    }
 
     @Around("allController()")
     public Object aroundServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
         Signature signature = joinPoint.getSignature();
         String fileFullName = signature.getDeclaringTypeName(); // 파일 전체 이름을 가져옵니다.
         String packageName = fileFullName.substring(0, fileFullName.lastIndexOf('.')); // 패키지 이름을 가져옵니다.
-        String className = fileFullName.substring(fileFullName.lastIndexOf('.')+1); // 클래스 이름을 가져옵니다.
+        String className = fileFullName.substring(fileFullName.lastIndexOf('.') + 1); // 클래스 이름을 가져옵니다.
         String methodName = signature.getName(); // 메서드 이름을 가져옵니다.
 
         try {
@@ -39,7 +43,7 @@ public class Logging {
             log.info("Method " + signature.toShortString() + " execution finished with duration " + executionTime + " ms");
 
             return result;
-        } catch (Throwable e){
+        } catch (Throwable e) {
             log.error("Method " + signature.toShortString() + " execution finished with error: " + e);
 
             throw e;
@@ -48,8 +52,11 @@ public class Logging {
 
     /*일단은 레포지토리 메서드가 정상적으로 동작하면 DB에 반영됨을 전제로 함.*/
     @Pointcut("execution(* com.kernel360.kernelsquare.domain.member.repository.MemberRepository.*(..)) || " +
-        "execution(* com.kernel360.kernelsquare.domain.question.repository.QuestionRepository.*(..))")
-    public void allRepository() {}
+            "execution(* com.kernel360.kernelsquare.domain.question.repository.QuestionRepository.*(..)) || " +
+            "execution(* com.kernel360.kernelsquare.domain.answer.repository.AnswerRepository.*(..)) || " +
+            "execution(* com.kernel360.kernelsquare.domain.member_answer_vote.repository.MemberAnswerVoteRepository.*(..))")
+    public void allRepository() {
+    }
 
     @Around("allRepository()")
     public Object aroundRepositoryMethod(ProceedingJoinPoint joinPoint) throws Throwable {

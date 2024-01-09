92a91bf71dad975f9621a23b7beb3a5024591609
mooncw <dusrktl4@gmail.com>
2024-01-09 15:20:59 +0900

    [feat] QueryDSL gradle 세팅
    - 빌드 전 커밋
    - 엘라스틱 서치 미사용으로 인한 관련 코드 제거

--- src/main/java/com/kernel360/kernelsquare/domain/search/controller/SearchController.java
+++ src/main/java/com/kernel360/kernelsquare/domain/search/controller/SearchController.java
@@ -1,25 +1,25 @@
-package com.kernel360.kernelsquare.domain.search.controller;
-
-import com.kernel360.kernelsquare.domain.search.entity.SearchItem;
-import com.kernel360.kernelsquare.domain.search.service.SearchService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1")
-@RequiredArgsConstructor
-public class SearchController {
-    private final SearchService searchService;
-
-    @GetMapping("/search")
-    public ResponseEntity<List<SearchItem>> searchQuestions(@RequestParam String query) {
-        List<SearchItem> searchResults = searchService.search(query);
-        return ResponseEntity.ok(searchResults);
-    }
-}
+//package com.kernel360.kernelsquare.domain.search.controller;
+//
+//import com.kernel360.kernelsquare.domain.search.entity.SearchItem;
+//import com.kernel360.kernelsquare.domain.search.service.SearchService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1")
+//@RequiredArgsConstructor
+//public class SearchController {
+//    private final SearchService searchService;
+//
+//    @GetMapping("/search")
+//    public ResponseEntity<List<SearchItem>> searchQuestions(@RequestParam String query) {
+//        List<SearchItem> searchResults = searchService.search(query);
+//        return ResponseEntity.ok(searchResults);
+//    }
+//}
--- src/main/java/com/kernel360/kernelsquare/domain/search/service/SearchService.java
+++ src/main/java/com/kernel360/kernelsquare/domain/search/service/SearchService.java
@@ -1,18 +1,18 @@
-package com.kernel360.kernelsquare.domain.search.service;
-
-import com.kernel360.kernelsquare.domain.search.entity.SearchItem;
-import com.kernel360.kernelsquare.domain.search.repository.SearchRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class SearchService {
-    private final SearchRepository searchRepository;
-
-    public List<SearchItem> search(String query) {
-        return searchRepository.findByTitleContainingOrContentContainingOrTechStackContaining(query, query, query);
-    }
-}
+//package com.kernel360.kernelsquare.domain.search.service;
+//
+//import com.kernel360.kernelsquare.domain.search.entity.SearchItem;
+//import com.kernel360.kernelsquare.domain.search.repository.SearchRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class SearchService {
+//    private final SearchRepository searchRepository;
+//
+//    public List<SearchItem> search(String query) {
+//        return searchRepository.findByTitleContainingOrContentContainingOrTechStackContaining(query, query, query);
+//    }
+//}

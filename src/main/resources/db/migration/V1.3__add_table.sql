f46b2696dc133dbeb680a7ff25b084b538b78de2
mooncw <dusrktl4@gmail.com>
2024-01-17 08:16:45 +0900

    [feat] 채팅방 테이블 추가

---
+++ src/main/resources/db/migration/V1.3__add_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `chat_room`;
+
+CREATE TABLE `chat_room` (
+    `created_date` datetime NOT NULL,
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `room_key` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `modified_date` datetime DEFAULT NULL,
+    `room_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

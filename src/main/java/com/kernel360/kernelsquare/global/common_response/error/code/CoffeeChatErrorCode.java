8bcc97448ac6fb49d6a48260cf4997784fe34fd8
mooncw <dusrktl4@gmail.com>
2024-01-22 15:39:47 +0900

    [refactor] 존재하지 않는 채팅방일 때 400 -> 404에러로 수정

--- src/main/java/com/kernel360/kernelsquare/global/common_response/error/code/CoffeeChatErrorCode.java
+++ src/main/java/com/kernel360/kernelsquare/global/common_response/error/code/CoffeeChatErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CoffeeChatErrorCode implements ErrorCode {
     MESSAGE_TYPE_NOT_VALID(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.MESSAGE_TYPE_NOT_VALID, "유효하지 않는 메시지 타입"),
-    COFFEE_CHAT_ROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.COFFEE_CHAT_ROOM_NOT_FOUND, "존재하지 않는 채팅방"),
+    COFFEE_CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, CoffeeChatServiceStatus.COFFEE_CHAT_ROOM_NOT_FOUND, "존재하지 않는 채팅방"),
     COFFEE_CHAT_ROOM_NOT_ACTIVE(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.COFFEE_CHAT_ROOM_NOT_ACTIVE, "비활성화 상태의 채팅방"),
     MENTOR_MISMATCH(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.MENTOR_MISMATCH, "해당 예약창을 만든 멘토와 불일치"),
     MEMBER_MISMATCH(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.MEMBER_MISMATCH, "해당 예약 시간의 멤버와 불일치"),

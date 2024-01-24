640138dcefa65d0db6de3f4a7a3aedd45228db2f
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-01-24 10:49:39 +0900

    #135 해시태그 Read, Delete 코드리뷰 반영2

--- src/main/java/com/kernel360/kernelsquare/domain/hashtag/dto/FindAllHashtagResponse.java
+++ src/main/java/com/kernel360/kernelsquare/domain/hashtag/dto/FindAllHashtagResponse.java
@@ -1,13 +1,11 @@
 package com.kernel360.kernelsquare.domain.hashtag.dto;
 
-import com.kernel360.kernelsquare.domain.hashtag.entity.Hashtag;
-
 import java.util.List;
 
 public record FindAllHashtagResponse(
-        List<HashtagResponse> hashtags
+        List<FindHashtagResponse> hashtags
 ) {
-    public static FindAllHashtagResponse from(List<HashtagResponse> hashtags) {
+    public static FindAllHashtagResponse from(List<FindHashtagResponse> hashtags) {
         return new FindAllHashtagResponse(hashtags);
     }
 }
--- src/main/java/com/kernel360/kernelsquare/domain/hashtag/dto/FindHashtagResponse.java
+++ src/main/java/com/kernel360/kernelsquare/domain/hashtag/dto/FindHashtagResponse.java
@@ -5,12 +5,12 @@ import com.kernel360.kernelsquare.domain.hashtag.entity.Hashtag;
 /**
  * DTO for {@link com.kernel360.kernelsquare.domain.hashtag.entity.Hashtag}
  */
-public record HashtagResponse(
+public record FindHashtagResponse(
         Long id,
         String content
 ) {
-    public static HashtagResponse from(Hashtag hashtag) {
-        return new HashtagResponse(
+    public static FindHashtagResponse from(Hashtag hashtag) {
+        return new FindHashtagResponse(
                 hashtag.getId(),
                 hashtag.getContent()
         );

e80165d54994c1bdc37d4b76347e2de94c16ed3f
hodako <kbry12@naver.com>
2024-01-29 12:46:08 +0900

    module : admin-api 추가

--- src/main/java/com/kernel360/kernelsquare/domain/reservation_article/repository/ReservationArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kernel360.kernelsquare.domain.reservation_article.repository;
-
-import com.kernel360.kernelsquare.domain.reservation_article.entity.ReservationArticle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationArticleRepository extends JpaRepository<ReservationArticle, Long> {
-
-}
--- src/main/java/com/kernel360/kernelsquare/global/common_response/service/code/ServiceStatus.java
+++
@@ -1,5 +0,0 @@
-package com.kernel360.kernelsquare.global.common_response.service.code;
-
-public interface ServiceStatus {
-	Integer getServiceStatus();
-}

3f822c1e339aeba7529a199f22e9bdef63857551
hodako <kbry12@naver.com>
2024-02-05 12:45:40 +0900

    refactor : 싱글 모듈 삭제

--- src/main/java/com/kernel360/kernelsquare/global/constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.kernel360.kernelsquare.global.constants;
-
-public class SecurityConstants {
-	public static final String TOKEN_PREFIX = "Bearer";
-	public static final String AUTHORIZATION_HEADER = "Authorization";
-}

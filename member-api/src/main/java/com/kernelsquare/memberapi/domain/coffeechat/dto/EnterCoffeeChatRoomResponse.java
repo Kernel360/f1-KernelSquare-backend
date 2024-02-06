e4567f506b8153b7762e9c2398909a3b4149fbc9
mooncw <dusrktl4@gmail.com>
2024-02-06 14:15:09 +0900

    [bugfix] 채팅방 입장 응답에서 exprirationTime에 @JsonFormat 추가

--- member-api/src/main/java/com/kernelsquare/memberapi/domain/coffeechat/dto/EnterCoffeeChatRoomResponse.java
+++ member-api/src/main/java/com/kernelsquare/memberapi/domain/coffeechat/dto/EnterCoffeeChatRoomResponse.java
@@ -1,5 +1,6 @@
 package com.kernelsquare.memberapi.domain.coffeechat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kernelsquare.domainmysql.domain.coffeechat.entity.ChatRoom;
 
 import lombok.Builder;
@@ -9,8 +10,12 @@ import java.time.LocalDateTime;
 @Builder
 public record EnterCoffeeChatRoomResponse(
 	String articleTitle,
+
 	String roomKey,
+
 	Boolean active,
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
 	LocalDateTime expirationTime
 ) {
 	public static EnterCoffeeChatRoomResponse of(String articleTitle, ChatRoom chatRoom) {

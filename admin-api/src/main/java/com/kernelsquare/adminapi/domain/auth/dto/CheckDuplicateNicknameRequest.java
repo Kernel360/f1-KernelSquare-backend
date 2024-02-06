5b992b5476f9be325e2767306846159651df337f
hodako <kbry12@naver.com>
2024-02-06 17:06:51 +0900

    refactor : 회원과 관리자 모듈에 있는 CheckDuplicateNicknameRequest 모두 수정

--- admin-api/src/main/java/com/kernelsquare/adminapi/domain/auth/dto/CheckDuplicateNicknameRequest.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/domain/auth/dto/CheckDuplicateNicknameRequest.java
@@ -1,13 +1,17 @@
 package com.kernelsquare.adminapi.domain.auth.dto;
 
+import com.kernelsquare.core.validation.ValidationGroups;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CheckDuplicateNicknameRequest(
-	@NotBlank(message = "닉네임을 입력해 주세요.")
-	@Size(min = 2, max = 8, message = "닉네임 길이를 확인해 주세요.")
+	@NotBlank(message = "닉네임을 입력해 주세요.", groups = ValidationGroups.NotBlankGroup.class)
+	@Size(min = 2, max = 8, message = "닉네임 길이를 확인해 주세요.", groups = ValidationGroups.SizeGroup.class)
+	@Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "완전한 한글 조합 또는 영문 대소문자만 입력하세요.", groups = ValidationGroups.PatternGroup.class)
 	String nickname
 ) {
 }

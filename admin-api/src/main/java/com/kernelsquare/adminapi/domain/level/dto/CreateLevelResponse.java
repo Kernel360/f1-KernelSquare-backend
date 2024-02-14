d9b5b5090e8767ed875483b4fa8f16c15456eef2
YunNote <zzdd1558@gmail.com>
2024-02-14 22:57:47 +0900

    feature: levelId만 사용한다면 Level보다는 ID만 넘겨보는건 어떨까요?

--- admin-api/src/main/java/com/kernelsquare/adminapi/domain/level/dto/CreateLevelResponse.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/domain/level/dto/CreateLevelResponse.java
@@ -5,6 +5,8 @@ import com.kernelsquare.domainmysql.domain.level.entity.Level;
 public record CreateLevelResponse(
 	Long levelId
 ) {
+
+	//
 	public static CreateLevelResponse from(Level level) {
 		return new CreateLevelResponse(
 			level.getId()

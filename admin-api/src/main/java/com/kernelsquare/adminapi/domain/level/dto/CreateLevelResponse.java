17081a12c69d4255c5dacd7c2b09b74a8d990184
YunNote <zzdd1558@gmail.com>
2024-02-14 22:59:26 +0900

    feature: 과도한 줄바꿈

--- admin-api/src/main/java/com/kernelsquare/adminapi/domain/level/dto/CreateLevelResponse.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/domain/level/dto/CreateLevelResponse.java
@@ -2,14 +2,13 @@ package com.kernelsquare.adminapi.domain.level.dto;
 
 import com.kernelsquare.domainmysql.domain.level.entity.Level;
 
-public record CreateLevelResponse(
-	Long levelId
-) {
+// 과도한 줄바꿈
+public record CreateLevelResponse(Long levelId) {
 
-	//
-	public static CreateLevelResponse from(Level level) {
-		return new CreateLevelResponse(
-			level.getId()
-		);
-	}
+   //
+   public static CreateLevelResponse from(Level level) {
+      return new CreateLevelResponse(
+         level.getId()
+      );
+   }
 }

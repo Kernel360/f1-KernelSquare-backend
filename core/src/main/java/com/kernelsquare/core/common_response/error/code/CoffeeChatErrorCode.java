6acb9fb025276ad8a3691146d6fd6c768805f31e
mooncw <dusrktl4@gmail.com>
2024-02-20 23:29:22 +0900

    [chore] 안쓰는 커스텀 에러 코드 삭제

--- core/src/main/java/com/kernelsquare/core/common_response/error/code/CoffeeChatErrorCode.java
+++ core/src/main/java/com/kernelsquare/core/common_response/error/code/CoffeeChatErrorCode.java
@@ -13,8 +13,6 @@ public enum CoffeeChatErrorCode implements ErrorCode {
 	COFFEE_CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, CoffeeChatServiceStatus.COFFEE_CHAT_ROOM_NOT_FOUND, "존재하지 않는 채팅방"),
 	COFFEE_CHAT_ROOM_NOT_ACTIVE(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.COFFEE_CHAT_ROOM_NOT_ACTIVE,
 		"비활성화 상태의 채팅방"),
-	MENTOR_MISMATCH(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.MENTOR_MISMATCH, "해당 예약창을 만든 멘토와 불일치"),
-	MEMBER_MISMATCH(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.MEMBER_MISMATCH, "해당 예약 시간의 멤버와 불일치"),
 	AUTHORITY_NOT_VALID(HttpStatus.BAD_REQUEST, CoffeeChatServiceStatus.AUTHORITY_NOT_VALID, "유효하지 않는 권한"),
 	MESSAGE_DELIVERY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, CoffeeChatServiceStatus.MESSAGE_DELIVERY_FAILED,
 		"토픽으로 메시지 전달 실패"),

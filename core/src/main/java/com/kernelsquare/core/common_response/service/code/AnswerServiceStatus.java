bfeb6022214fcc22fa4a71ce311d11e2cc69bbce
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-02-20 11:23:52 +0900

    #265 본인 질문에 답변X 로직 추가

--- core/src/main/java/com/kernelsquare/core/common_response/service/code/AnswerServiceStatus.java
+++ core/src/main/java/com/kernelsquare/core/common_response/service/code/AnswerServiceStatus.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum AnswerServiceStatus implements ServiceStatus {
+	// error
 	ANSWER_CREATION_NOT_AUTHORIZED(2200),
 	ANSWER_UPDATE_NOT_AUTHORIZED(2201),
 	ANSWER_NOT_FOUND(2202),
+	ANSWER_SELF_IMPOSSIBLE(2203),
 
+	// success
 	ANSWER_CREATED(2240),
 	ANSWERS_ALL_FOUND(2241),
 	ANSWER_UPDATED(2242),

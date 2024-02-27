8c76a03775ab11404a11eb0ea9d9952e1bc1d8d1
Wonsang Kim <rosskysoss@gmail.com>
2024-02-27 14:21:12 +0900

    [chore] 중복 getter 삭제

--- core/src/main/java/com/kernelsquare/core/type/CodingMeetingReadType.java
+++ core/src/main/java/com/kernelsquare/core/type/CodingMeetingReadType.java
@@ -3,7 +3,6 @@ package com.kernelsquare.core.type;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
 public enum CodingMeetingReadType {
     ALL("all"), OPEN("open"), CLOSED("closed");

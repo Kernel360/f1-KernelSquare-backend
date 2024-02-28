88a5770808c4f9fbf42fffbcb337e77a09ebd3d3
Wonsang Kim <rosskysoss@gmail.com>
2024-02-28 12:52:00 +0900

    [fix] ImageUrl 유틸 함수 추가

--- domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/coding_meeting_comment/info/CodingMeetingCommentListInfo.java
+++ domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/coding_meeting_comment/info/CodingMeetingCommentListInfo.java
@@ -1,5 +1,6 @@
 package com.kernelsquare.domainmysql.domain.coding_meeting_comment.info;
 
+import com.kernelsquare.core.util.ImageUtilsCore;
 import com.kernelsquare.domainmysql.domain.coding_meeting_comment.entity.CodingMeetingComment;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,8 +23,8 @@ public class CodingMeetingCommentListInfo {
         this.memberId = codingMeetingComment.getMember().getId();
         this.memberLevel = codingMeetingComment.getMember().getLevel().getName();
         this.memberNickname = codingMeetingComment.getMember().getNickname();
-        this.memberProfileUrl = codingMeetingComment.getMember().getImageUrl();
-        this.memberLevelImageUrl = codingMeetingComment.getMember().getLevel().getImageUrl();
+        this.memberProfileUrl = ImageUtilsCore.makeImageUrl(codingMeetingComment.getMember().getImageUrl());
+        this.memberLevelImageUrl = ImageUtilsCore.makeImageUrl(codingMeetingComment.getMember().getLevel().getImageUrl());
         this.createdDate = codingMeetingComment.getCreatedDate();
         this.codingMeetingCommentToken = codingMeetingComment.getCodingMeetingCommentToken();
         this.codingMeetingCommentContent = codingMeetingComment.getCodingMeetingCommentContent();

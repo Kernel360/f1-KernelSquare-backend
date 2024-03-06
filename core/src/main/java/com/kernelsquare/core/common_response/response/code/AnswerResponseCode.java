96e84655061163349476b458fd634577d4c4a3ec
hodako <kbry12@naver.com>
2024-03-06 18:09:44 +0900

    feat: 예외처리 및 정상 응답 추가

--- core/src/main/java/com/kernelsquare/core/common_response/response/code/AnswerResponseCode.java
+++ core/src/main/java/com/kernelsquare/core/common_response/response/code/AnswerResponseCode.java
@@ -12,7 +12,8 @@ public enum AnswerResponseCode implements ResponseCode {
 	ANSWER_CREATED(HttpStatus.OK, AnswerServiceStatus.ANSWER_CREATED, "답변 생성 성공"),
 	ANSWERS_ALL_FOUND(HttpStatus.OK, AnswerServiceStatus.ANSWERS_ALL_FOUND, "질문에 대한 모든 답변 조회 성공"),
 	ANSWER_UPDATED(HttpStatus.OK, AnswerServiceStatus.ANSWER_UPDATED, "답변 수정 성공"),
-	ANSWER_DELETED(HttpStatus.OK, AnswerServiceStatus.ANSWER_DELETED, "답변 삭제 성공");
+	ANSWER_DELETED(HttpStatus.OK, AnswerServiceStatus.ANSWER_DELETED, "답변 삭제 성공"),
+	AUTOMATED_ANSWER_CREATED(HttpStatus.OK, AnswerServiceStatus.AUTOMATED_ANSWER_CREATED, "커널스퀘어 AI 인턴 답변 성공");
 
 	private final HttpStatus code;
 	private final ServiceStatus serviceStatus;
--- core/src/main/java/com/kernelsquare/core/common_response/service/code/AnswerServiceStatus.java
+++ core/src/main/java/com/kernelsquare/core/common_response/service/code/AnswerServiceStatus.java
@@ -13,7 +13,8 @@ public enum AnswerServiceStatus implements ServiceStatus {
 	ANSWER_CREATED(2240),
 	ANSWERS_ALL_FOUND(2241),
 	ANSWER_UPDATED(2242),
-	ANSWER_DELETED(2243);
+	ANSWER_DELETED(2243),
+	AUTOMATED_ANSWER_CREATED(2244);
 
 	private final Integer code;
 

90eed0f3bae0275b7bcc651271796c05c38121c8
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-03-11 02:50:32 +0900

    feat: 소셜로그인#177 * 테스트 에러 부분 수정

--- member-api/src/main/java/com/kernelsquare/memberapi/domain/auth/dto/LoginResponse.java
+++ member-api/src/main/java/com/kernelsquare/memberapi/domain/auth/dto/LoginResponse.java
@@ -6,7 +6,6 @@ import com.kernelsquare.core.type.AuthorityType;
 import com.kernelsquare.domainmysql.domain.authority.entity.Authority;
 import com.kernelsquare.domainmysql.domain.member.entity.Member;
 import com.kernelsquare.domainmysql.domain.member_authority.entity.MemberAuthority;
-import com.kernelsquare.memberapi.domain.image.utils.ImageUtils;
 import lombok.Builder;
 
 import java.util.List;

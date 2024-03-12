edb9e68825fec6d1c52f11e17020d2058b8176e4
mooncw <dusrktl4@gmail.com>
2024-03-12 15:25:42 +0900

    fix: 답변 작성자만 수정 삭제 가능하게 수정

--- domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/answer/repository/AnswerReader.java
+++ domain-mysql/src/main/java/com/kernelsquare/domainmysql/domain/answer/repository/AnswerReader.java
@@ -5,6 +5,8 @@ import com.kernelsquare.domainmysql.domain.answer.entity.Answer;
 import java.util.List;
 
 public interface AnswerReader {
+    Answer findAnswer(Long answerId);
+
     List<Answer> findAnswers(Long questionId);
 
     List<Answer> findAnswersTop3(Long questionId);
